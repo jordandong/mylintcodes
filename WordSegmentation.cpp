5cbbba40ea4db161008360bbbfedd69341bf9998
jordandong <jordandong@gmail.com>
2014-12-20 14:18:52 -0800

    20141220v1

---
+++ WordSegmentation.cpp
@@ -0,0 +1,48 @@
+/*
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Example
+Given
+
+s = "lintcode",
+
+dict = ["lint", "code"].
+
+Return true because "lintcode" can be segmented as "lint code".
+*/
+
+class Solution {
+public:
+    /**
+     * @param s: A string s
+     * @param dict: A dictionary of words dict
+     */
+    bool wordSegmentation(string s, unordered_set<string> &dict) {
+        // write your code here
+        int sz = s.length();
+        bool dp[sz+1];
+        char chrs[256] = {0};
+       
+        for(string word : dict) {
+            for(char c : word) {
+                chrs[c]++;
+            }
+        }
+        for(char c : s) {
+            if (chrs[c] == 0)
+              return false;
+        } 
+  
+        memset(dp, 0, (sz+1)*sizeof(bool));
+        dp[0] = true;
+        for(int i = 0; i<sz; i++){
+            for(int j = i; j>=0; j--){
+                if(dict.count(s.substr(j, i-j+1)) && dp[j]==true){
+                    dp[i+1] = true;
+                    break;
+                }
+            }
+        }
+        return dp[sz];
+    }
+};
