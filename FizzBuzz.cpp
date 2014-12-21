c86bf398922a6764e1daf5d699a5a0623cc72c4d
jordandong <jordandong@gmail.com>
2014-12-20 18:59:51 -0800

    20141220v4

---
+++ FizzBuzz.cpp
@@ -0,0 +1,35 @@
+/*
+Given number n. Print number from 1 to n. But:
+
+when number is divided by 3, print fizz;
+
+when number is divided by 5, print buzz;
+
+when number is divided by both 3 and 5, print fizz buzz.
+
+Example
+If n=15, you should return ["1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizz buzz"]
+*/
+
+class Solution {
+public:
+    /**
+     * param n: As description.
+     * return: A list of strings.
+     */
+    vector<string> fizzBuzz(int n) {
+        vector<string> results;
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                results.push_back("fizz buzz");
+            } else if (i % 5 == 0) {
+                results.push_back("buzz");
+            } else if (i % 3 == 0) {
+                results.push_back("fizz");
+            } else {
+                results.push_back(to_string(i));
+            }
+        }
+        return results;
+    }
+};
