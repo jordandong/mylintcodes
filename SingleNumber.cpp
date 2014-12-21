c7932b2729deb4355fb647e50853c9a66d085671
jordandong <jordandong@gmail.com>
2014-12-20 18:56:41 -0800

    20141220v3

---
+++ SingleNumber.cpp
@@ -0,0 +1,26 @@
+/*
+Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
+
+Example
+Given [1,2,2,1,3,4,3], return 4
+
+Challenge
+One-pass, constant extra space
+*/
+
+
+class Solution {
+public:
+	/**
+	 * @param A: Array of integers.
+	 * return: The single number.
+	 */
+    int singleNumber(vector<int> &A) {
+        // write your code here
+        int r = 0;
+        for(int e : A){
+            r^=e;
+        }
+        return r;
+    }
+};
