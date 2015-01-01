b81440bc617a0948ffb9d96053efa2b909f27953
jordandong <jordandong@gmail.com>
2014-12-31 16:58:53 -0800

    20141231v1

---
+++ RemoveDuplicatesFromSortedList.cpp
@@ -0,0 +1,39 @@
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: head node
+     */
+    ListNode *deleteDuplicates(ListNode *head) {
+        // write your code here
+        ListNode *cur = head;
+        while(cur){
+            if(cur->next && cur->val == cur->next->val){
+                cur->next = cur->next->next;
+            }else{
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
