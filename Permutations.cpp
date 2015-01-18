a36c0348b35e388abd9ae10b40b4628c07cc84e9
jordandong <jordandong@gmail.com>
2015-01-18 13:40:16 -0800

    20150118_v4_1

--- Permutations.cpp
+++ Permutations.cpp
@@ -17,6 +17,7 @@ Challenge:
 Do it without recursion
 */
 
+//Without Recursion
 class Solution {
 public:
     /**
@@ -51,5 +52,36 @@ public:
         swap(nums[i], nums[j]);
         reverse(nums.begin() + i +1, nums.end());
         return true;
+    }
+};
+
+
+//Recursion
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    vector<vector<int> > permute(vector<int> nums) {
+        // write your code here
+        vector<vector<int> > res;
+        permuteHelper(nums, 0, res);
+        return res;
+    }
+    
+    void permuteHelper(vector<int> &nums, int start, vector<vector<int> > &res){
+        int sz = nums.size();
+        if(start == sz){
+            if(sz != 0)
+                res.push_back(nums);
+            return;
+        }
+        
+        for(int i = start; i < sz; i++){
+            swap(nums[start], nums[i]);
+            permuteHelper(nums, start+1, res);
+            swap(nums[i], nums[start]);
+        }
     }
 };
