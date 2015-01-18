91086011fa3674e99bb73f87c4773c7ed46f9a4f
jordandong <jordandong@gmail.com>
2015-01-18 14:35:14 -0800

    20150118_v5_1

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -11,7 +11,6 @@ Challenge:
 Reverse it in-place and in one-pass
 */
 
-//two pass
 /**
  * Definition of singly-linked-list:
  * 
@@ -25,6 +24,56 @@ Reverse it in-place and in one-pass
  *     }
  * }
  */
+ 
+//One Pass 
+class Solution {
+public:
+    /**
+     * @param head: The head of linked list.
+     * @param m: The start position need to reverse.
+     * @param n: The end position need to reverse.
+     * @return: The new head of partial reversed linked list.
+     */
+    ListNode *reverseBetween(ListNode *head, int m, int n) {
+        // write your code here
+        ListNode* prehead = new ListNode(-1);
+        ListNode* prehead_tail = NULL;
+        ListNode* rev_head = NULL;
+        ListNode* rev_tail = NULL;
+        ListNode* tail = NULL;
+        ListNode* cur = prehead;
+        prehead->next = head;
+        
+        int cnt = 0;
+        while(cur){
+            ListNode* tmp = cur;
+            if(cnt + 1 == m){
+                rev_head = cur->next;
+                prehead_tail = cur;
+            }
+            if(cnt == n)
+                tail = cur->next;
+            cur = cur->next;
+            if(cnt + 1 == m || cnt == n)
+                tmp->next = NULL;
+                
+            if(cnt >=m && cnt<=n){
+                tmp->next = rev_tail;
+                rev_tail = tmp;
+            }
+            cnt++;
+        }
+        
+        prehead_tail->next = rev_tail;
+        rev_head->next = tail;
+        
+        cur = prehead->next;
+        delete prehead;
+        return cur;
+    }
+};
+
+//Two Pass
 class Solution {
 public:
     /**
