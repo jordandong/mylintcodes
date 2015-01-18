5ea460cc60c2324dc11c4722de6281a5883aeb03
jordandong <jordandong@gmail.com>
2015-01-18 13:50:09 -0800

    20150118_v3_1

--- UniquePermutations.cpp
+++ UniquePermutations.cpp
@@ -14,6 +14,7 @@ Challenge:
 Do it without recursion.
 */
 
+//Without Recursion
 class Solution {
 public:
     /**
@@ -47,6 +48,46 @@ public:
         
         swap(nums[i], nums[j]);
         reverse(nums.begin() + i +1, nums.end());
+        return true;
+    }
+};
+
+//Recursion
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of unique permutations.
+     */
+    vector<vector<int> > permuteUnique(vector<int> &nums) {
+        // write your code here
+        vector<vector<int> > res;
+        permuteUniqueHelper(nums, 0, res);
+        return res;
+    }
+    
+    void permuteUniqueHelper(vector<int> &nums, int start, vector<vector<int> > &res){
+        int sz = nums.size();
+        if(start == sz){
+            if(sz != 0)
+                res.push_back(nums);
+            return;
+        }
+        
+        for(int i = start; i < sz; i++){
+            if(do_swap(nums, start, i)){
+                swap(nums[start], nums[i]);
+                permuteUniqueHelper(nums, start+1, res);
+                swap(nums[i], nums[start]);
+            }
+        }
+    }
+    
+    bool do_swap(vector<int>&nums, int start, int end){
+        while(start < end){
+            if(nums[start++] == nums[end])
+                return false;
+        }
         return true;
     }
 };
