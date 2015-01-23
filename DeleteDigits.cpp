52ee88267ffaf904c2a681e3cc7db6482086346f
jordandong <jordandong@gmail.com>
2015-01-23 01:37:43 -0800

    20150123

---
+++ DeleteDigits.cpp
@@ -0,0 +1,45 @@
+/*
+Given string A representative a positive integer which has N digits, remove any k digits of the number, the remaining digits are arranged according to the original order to become a new minimum positive integer. 
+N <= 240 and k <=N, 
+Example:
+Given an integer A = 178542, k = 4
+return a string "12"
+
+Given an integer A = 90123549, k = 2
+return a string "12349"
+*/
+
+class Solution {
+public:
+    /**
+     *@param A: A positive integer which has N digits, A is a string.
+     *@param k: Remove k digits.
+     *@return: A string
+     */
+    string DeleteDigits(string A, int k){
+        // wirte your code here
+        int sz = A.size();
+        int i = 0;
+        while(k){
+            i = 0;
+            while(i < sz - 1 && A[i] <= A[i+1]) // find the first one bigger than the right digit
+                i++;
+            if(i == sz - 1){ // in sorted order, delete the tailing digits
+                sz -= k;
+                k = 0;
+            }else{
+                while(i < sz -1){ // remove the found digit
+                    A[i] = A[i+1];
+                    i++;
+                }
+                k--;
+                sz--;
+            }
+        }
+        
+        i = 0;
+        while(i < sz && A[i] == '0') // deleting beginning zeors
+            i++;
+        return A.substr(i, sz - i);
+    }
+};
