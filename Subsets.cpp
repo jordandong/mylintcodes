e4d3a8ac01198782de403a3dc6b591c399550306
jordandong <jordandong@gmail.com>
2015-02-15 14:25:36 -0800

    201502115

--- Subsets.cpp
+++ Subsets.cpp
@@ -27,13 +27,13 @@ public:
     vector<vector<int> > subsets(vector<int> &nums) {
     	// write your code here
     	int n = nums.size();
-    	int mx = 1<<n;
+    	int mx = 1 << n;
     	vector<vector<int> > res;
-
+    	sort(nums.begin(), nums.end());//need sort to make sure the order is correct
     	for(int i = 0; i < mx; i++){
     	    vector<int> sol;
     	    for(int shift = 0; shift < n; shift++){
-    	        if((i>>shift) & 0x00000001){
+    	        if((i>>shift) & 0x1){
     	            sol.push_back(nums[shift]);
     	        }
     	    }
