ac8b592fd388e3b98eea79860d4ea88427844be7
jordandong <jordandong@gmail.com>
2015-04-20 14:45:52 -0700

    20150420v2

--- RouteBetweenTwoNodesInGraph.cpp
+++ RouteBetweenTwoNodesInGraph.cpp
@@ -26,6 +26,8 @@ Cracking The Coding Interview Depth First Search Breadth First Search
  *     DirectedGraphNode(int x) : label(x) {};
  * };
  */
+ 
+//DFS
 class Solution {
 public:
     /**
@@ -49,6 +51,41 @@ public:
             if(v.find(s->neighbors[i]) == v.end()){
                 if(hasRouteHelper(s->neighbors[i], t, v))
                     return true;	
+            }
+        }
+        return false;
+    }
+};
+
+//Why do we need the graph???????????
+//BFS
+class Solution {
+public:
+    /**
+     * @param graph: A list of Directed graph node
+     * @param s: the starting Directed graph node
+     * @param t: the terminal Directed graph node
+     * @return: a boolean value
+     */
+    bool hasRoute(vector<DirectedGraphNode*> graph,
+                  DirectedGraphNode* s, DirectedGraphNode* t) {
+        // write your code here
+        if(s == t)
+            return true;
+        unordered_map<DirectedGraphNode*, bool> v;
+        stack<DirectedGraphNode*> stk;
+        stk.push(s);
+        v[s] = true;
+        while(stk.size()){
+            DirectedGraphNode* node = stk.top();
+            stk.pop();
+            for(int i = 0; i < node->neighbors.size(); i++){
+                if(node->neighbors[i] == t)
+                    return true;
+                if(v.find(node->neighbors[i]) == v.end()){
+                    stk.push(node->neighbors[i]);
+                    v[node->neighbors[i]] = true;
+                }
             }
         }
         return false;
