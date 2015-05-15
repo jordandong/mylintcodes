b4770de5e9dbce9ba551d98bc6ef13ccc1c324d1
jordandong <jordandong@gmail.com>
2015-05-14 20:10:42 -0700

    20150514

--- FindPeakElementII.cpp
+++ FindPeakElementII.cpp
@@ -12,11 +12,11 @@ Find a peak element in this matrix. Return the index of the peak.
 Example
 Given a matrix:
 [
-  [1 ,2 ,3 ,4 ,5],
+  [1 ,2 ,3 ,6 ,5],
   [16,41,23,22,6],
   [15,17,24,21,7],
-  [14,18,19,20,8],
-  [13,12,11,10,9]
+  [14,18,19,20,10],
+  [13,14,11,10,9]
 ]
 return index of 41 (which is [1,1]) or index of 24 (which is [2,2])
 
@@ -24,15 +24,15 @@ Note
 The matrix may contains multiple peeks, find any of them.
 
 Challenge
-Solve it in O(n) time.
+Solve it in O(n+m) time.
 
-If you come up with an algorithm that you thought it is O(n log n), can you prove it is actually O(n) or propose a similar but O(n) algorithm?
+If you come up with an algorithm that you thought it is O(n log m) or O(m log n), can you prove it is actually O(n+m) or propose a similar but O(n+m) algorithm?
 
 Tags Expand 
 Binary Search LintCode Copyright Matrix
 */
 
-//It is O(n), Cannot Pass Case 15 LTE 
+//O(min(M, N)*log(max(M, N)))
 class Solution {
 public:
     /**
@@ -41,69 +41,96 @@ public:
      */
     vector<int> findPeakII(vector<vector<int> > A) {
         // write your code here
-        vector<int> res;
         int M = A.size();
         if (M == 0)
-            return res;
+            return {};
         int N = A[0].size();
         if (N == 0)
-            return res;
-        findPeakIIHelper(0, M - 1, 0, N - 1, A, res);
-        return res;
+            return {};
+
+        if (N > M) {
+            int lo = 0, hi = N - 1;
+            while (lo <= hi) { //O(logN)
+                int mid = lo + (hi - lo)/2;
+                int mx = A[0][mid];
+                int mx_i = 0;
+                for (int i = 0; i < M; i++) {//O(M)
+                    if (A[i][mid] > mx) {
+                        mx = A[i][mid];
+                        mx_i = i; 
+                    }
+                }
+                if(mid > 0 && A[mx_i][mid - 1] > mx)
+                    hi = mid - 1;
+                else if(mid < N - 1 && A[mx_i][mid + 1] > mx)
+                    lo = mid + 1;
+                else
+                    return {mx_i, mid};	
+            }
+        } else {
+            int lo = 0, hi = M - 1;
+            while (lo <= hi) { //O(logM)
+                int mid = lo + (hi - lo)/2;
+                int mx = A[mid][0];
+                int mx_j = 0;
+                for (int j = 0; j < N; j++) {//O(N)
+                    if (A[mid][j] > mx) {
+                        mx = A[mid][j];
+                        mx_j = j; 
+                    }
+                }
+                if(mid > 0 && A[mid - 1][mx_j] > mx)
+                    hi = mid - 1;
+                else if(mid < M - 1 && A[mid + 1][mx_j] > mx)
+                    lo = mid + 1;
+                else
+                    return {mid, mx_j};	
+            }
+        }
+    }
+};
+
+//O(M*N)
+class Solution {
+public:
+    /**
+     * @param A: An integer matrix
+     * @return: The index of the peak
+     */
+    vector<int> findPeakII(vector<vector<int> > A) {
+        // write your code here
+        int M = A.size();
+        if (M == 0)
+            return {};
+        int N = A[0].size();
+        if (N == 0)
+            return {};
+        int i = 0, j = 0;
+        while (true) {
+            if( is_bigger(i - 1, j, A[i][j], A) ) {
+                i--;
+                continue;
+            }
+            if( is_bigger(i + 1, j, A[i][j], A) ) {
+                i++;
+                continue;
+            }
+            if( is_bigger(i, j - 1, A[i][j], A) ) {
+                j--;
+                continue;
+            }
+            if( is_bigger(i, j + 1, A[i][j], A) ) {
+                j++;
+                continue;
+            }
+            break;
+        }
+        return {i, j};
     }
     
-    void findPeakIIHelper(int li, int hi, int lj, int hj, vector<vector<int> > &A, vector<int> &res) {
-        while(true) {
-        
-        if(hj == lj && hi == li){
-            res.push_back(li);
-            res.push_back(lj);
-            return;
-        }
-            
-        if(hj - lj >= hi - li){
-            int mid = lj + (hj - lj)/2;
-            int mx = A[li][mid];
-            int mx_i = li;
-            for (int i = li; i <= hi; i++) {
-                if (A[i][mid] > mx) {
-                    mx = A[i][mid];
-                    mx_i = i; 
-                }
-            }
-            if (mid > 0 && A[mx_i][mid - 1] > mx ) {
-                hj = mid - 1;
-                //findPeakIIHelper(li, hi, lj, mid - 1, A, res);
-            } else if (mid < A[0].size() - 1 && A[mx_i][mid + 1] > mx ) {
-                lj = mid + 1;
-                //findPeakIIHelper(li, hi, mid + 1, hj, A, res);
-            } else {
-                res.push_back(mx_i);
-                res.push_back(mid);
-                return;
-            }
-        }else{
-            int mid = li + (hi - li)/2;
-            int mx = A[mid][lj];
-            int mx_j = lj;
-            for (int j = lj; j <= hj; j++) {
-                if (A[mid][j] > mx) {
-                    mx = A[mid][j];
-                    mx_j = j; 
-                }
-            }
-            if (mid > 0 && A[mid - 1][mx_j] > mx ) {
-                //findPeakIIHelper(li, mid - 1, lj, hj, A, res);
-                hi = mid - 1;
-            } else if (mid < A.size() - 1 && A[mid + 1][mx_j] > mx ){
-                //findPeakIIHelper(mid + 1, hi, lj, hj, A, res);
-                li = mid + 1;
-            } else {
-                res.push_back(mid);
-                res.push_back(mx_j);
-                return;
-            }
-        }
-        }
+    bool is_bigger(int i, int j, int val, vector<vector<int> > &A) {
+        if (i < 0 || i >= A.size() || j < 0 || j >= A[0].size())
+            return false;
+        return A[i][j] > val;
     }
 };
