b25f2f5fba989a15f94b83ce4629cdfcb8da3058
jordandong <jordandong@gmail.com>
2015-05-25 14:17:09 -0700

    20150525v4

--- PartitionArray.cpp
+++ PartitionArray.cpp
@@ -31,3 +31,26 @@ public:
         return lo;
     }
 };
+
+//min swaps
+class Solution {
+public:
+    int partitionArray(vector<int> &nums, int k) {
+        // write your code here
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            while (left <= right && nums[left] < k) {
+                ++left;
+            } 
+            while (left <= right && nums[right] >= k) {
+                --right;
+            }
+            if (left <= right) {
+               swap(nums[left++], nums[right--]);
+            }
+        }
+        return left;
+    }
+};
