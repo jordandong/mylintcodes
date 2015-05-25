18d0e076ca9dbeb79ba144dc6bb19ddd1d3a7185
jordandong <jordandong@gmail.com>
2015-05-24 17:04:04 -0700

    20150524v4

--- NextPermuation.cpp
+++ NextPermuation.cpp
@@ -2,12 +2,16 @@
 Given a list of integers, which denote a permutation.
 Find the next permutation in ascending order.
 
-Note:
-The list may contains duplicate integers.
-
-Example:
+Have you met this question in a real interview? Yes
+Example
 For [1,3,2,3], the next permutation is [1,3,3,2]
 For [4,3,2,1], the next permutation is [1,2,3,4]
+
+Note
+The list may contains duplicate integers.
+
+Tags Expand 
+LintCode Copyright Permutation
 */
 
 class Solution {
@@ -16,7 +20,7 @@ public:
      * @param nums: An array of integers
      * @return: An array of integers that's next permuation
      */
-    vector<int> nextPermuation(vector<int> &nums) {
+    vector<int> nextPermutation(vector<int> &nums) {
         // write your code here
         int sz = nums.size();
         int i = sz - 2;
