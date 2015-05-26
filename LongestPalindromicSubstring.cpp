a9818ae0e0192293fb94e5459dd6a1d0fbbd32a7
jordandong <jordandong@gmail.com>
2015-05-25 21:21:39 -0700

    20150525v11

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -12,6 +12,61 @@ Tags Expand
 String
 */
 
+//T:O(N) S:O(N)
+//http://articles.leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
+class Solution {
+public:
+    /**
+     * @param s input string
+     * @return the longest palindromic substring
+     */
+    string longestPalindrome(string& s) {
+        // Write your code here
+        string T = preProcess(s);
+        int n = T.length();
+        int P[n];
+        int C = 0, R = 0;
+        for (int i = 1; i < n-1; i++) {
+            int i_mirror = 2*C - i; // equals to i' = C - (i-C)
+            P[i] = (R > i) ? min(R - i, P[i_mirror]) : 0;
+            // Attempt to expand palindrome centered at i
+            while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
+                P[i]++;
+            // If palindrome centered at i expand past R,
+            // adjust center based on expanded palindrome.
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
+        }
+        
+        // Find the maximum element in P.
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n-1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+        return s.substr((centerIndex - 1 - maxLen)/2, maxLen);
+    }
+    
+    // Transform S into T.
+    // For example, S = "abba", T = "^#a#b#b#a#$".
+    // ^ and $ signs are sentinels appended to each end to avoid bounds checking
+    string preProcess(string s) {
+        int n = s.length();
+        if (n == 0)
+            return "^$";
+        string ret = "^";
+        for (int i = 0; i < n; i++)
+            ret += "#" + s.substr(i, 1);
+        ret += "#$";
+        return ret;
+    }
+};
+
 //S:O(N^2) T:O(N^2)
 class Solution {
 public:
