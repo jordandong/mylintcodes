a3651330503369a40523ea065bde9978f7c716e0
jordandong <jordandong@gmail.com>
2015-06-03 01:02:21 -0700

    20150603v10

--- SubsetsII.cpp
+++ SubsetsII.cpp
@@ -1,12 +1,8 @@
 /*
 Given a list of numbers that may has duplicate numbers, return all possible subsets
 
-Note:
-Each element in a subset must be in non-descending order.
-The ordering between two subsets is free.
-The solution set must not contain duplicate subsets.
-
-Example:
+Have you met this question in a real interview? Yes
+Example
 If S = [1,2,2], a solution is:
 
 [
@@ -17,6 +13,13 @@ If S = [1,2,2], a solution is:
   [1,2],
   []
 ]
+Note
+Each element in a subset must be in non-descending order.
+The ordering between two subsets is free.
+The solution set must not contain duplicate subsets.
+
+Tags Expand 
+Recursion
 */
 
 class Solution {
