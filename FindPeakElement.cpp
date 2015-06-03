066a8d8ce3e3f6e20f3e9354e6aa9d322eb60500
jordandong <jordandong@gmail.com>
2015-06-03 00:39:21 -0700

    20150603v4

--- FindPeakElement.cpp
+++ FindPeakElement.cpp
@@ -1,26 +1,27 @@
 /*
-Fair Find a Peak Show Result My Submissions
-
 There is an integer array which has the following features:
 
-    * The numbers in adjacent positions are different.
+The numbers in adjacent positions are different.
+A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+We define a position P is a peek if:
 
-    * A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+A[P] > A[P-1] && A[P] > A[P+1]
+Find a peak element in this array. Return the index of the peak.
 
-We define a position P is a peek if A[P] > A[P-1] && A[P] > A[P+1].
+Have you met this question in a real interview? Yes
+Example
+Given [1, 2, 1, 3, 4, 5, 7, 6]
 
-Find a peak in this array. Return the index of the peak.
+Return index 1 (which is number 2) or 6 (which is number 7)
 
 Note
 The array may contains multiple peeks, find any of them.
 
-Example
-[1, 2, 1, 3, 4, 5, 7, 6]
-
-return index 1 (which is number 2)  or 6 (which is number 7)
-
 Challenge
 Time complexity O(logN)
+
+Tags Expand 
+Binary Search LintCode Copyright Array
 */
 
 class Solution {
