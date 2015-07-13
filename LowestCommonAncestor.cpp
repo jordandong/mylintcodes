d547a4a5b57c1354ba8dbccdbe8665fc264e0d29
jordandong <jordandong@gmail.com>
2015-07-12 18:31:01 -0700

    20150712

--- LowestCommonAncestor.cpp
+++ LowestCommonAncestor.cpp
@@ -4,13 +4,9 @@ The lowest common ancestor is the node with largest depth which is the ancestor 
 
 Example:
         4
-
     /     \
-
   3         7
-
           /     \
-
         5         6
 For 3 and 5, the LCA is 4.
 For 5 and 6, the LCA is 7.
@@ -29,6 +25,44 @@ For 6 and 7, the LCA is 7.
  *     }
  * }
  */
+class Solution {
+public:
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *A, TreeNode *B) {
+        // write your code here
+        int lca_count = 0;
+        TreeNode* res = LCAHelper(root, A, B, lca_count);
+        return lca_count == 2 ? res:NULL;
+    }
+    
+    TreeNode* LCAHelper(TreeNode *root, TreeNode *A, TreeNode *B, int &lca_count){
+        if(!root)
+            return NULL;
+        
+        TreeNode* left_rtn = LCAHelper(root->left, A, B, lca_count);
+        if(lca_count == 2 && left_rtn) // avoid the unecessary search after we found two
+            return left_rtn;
+        TreeNode* right_rtn = LCAHelper(root->right, A, B, lca_count);
+        
+        if(left_rtn && right_rtn){
+            return root;
+        }else if(root == A || root == B){
+            if(root == A && root == B)
+                lca_count++;
+            lca_count++;
+            return root;
+        }else{
+            return left_rtn?left_rtn:right_rtn;
+        }    
+    }
+    
+};
+
+ //should be failed in certain cases since the value may be duplicated
 class Solution {
 public:
     /**
