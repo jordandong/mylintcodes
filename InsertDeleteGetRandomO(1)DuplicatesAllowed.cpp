c595aacbdd9309a01872270fcbc94ebd65fce2c7
J.D <jordandong@gmail.com>
2016-08-09 23:50:55 -0700

    Update InsertDeleteGetRandomO(1)DuplicatesAllowed.cpp

--- InsertDeleteGetRandomO(1)DuplicatesAllowed.cpp
+++ InsertDeleteGetRandomO(1)DuplicatesAllowed.cpp
@@ -65,3 +65,53 @@ public:
         return list_[rand() % list_.size()];
     }
 };
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
+class RandomizedCollection {
+private:
+    vector<int> list_;
+    unordered_multimap<int, int> used_;
+public:
+    /** Initialize your data structure here. */
+    RandomizedCollection() {
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    bool insert(int val) {
+        used_.emplace(val, list_.size());
+        list_.push_back(val);
+        return 1 == used_.count(val); 
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    bool remove(int val) {
+        if (0 == used_.count(val))
+            return false;
+
+        auto it_to_delete = used_.find(val);
+        auto it_to_back = used_.find(list_.back());
+        it_to_back->second = it_to_delete->second;
+        swap(list_[it_to_delete->second], list_.back());
+        used_.erase(it_to_delete);
+        list_.pop_back();
+        return true;  
+    }
+    
+    /** Get a random element from the collection. */
+    int getRandom() {
+        return list_[rand() % list_.size()];
+    }
+};
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
