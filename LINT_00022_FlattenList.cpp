f334db9a1e08b71d06547faf4a2175f3b6e15d6e
J.D <jordandong@gmail.com>
2016-08-24 14:24:38 -0700

    Update LINT_00022_FlattenList.cpp

--- LINT_00022_FlattenList.cpp
+++ LINT_00022_FlattenList.cpp
@@ -61,3 +61,23 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    // @param nestedList a list of NestedInteger
+    // @return a list of integer
+    vector<int> flatten(vector<NestedInteger> &nestedList) {
+        // Write your code here
+        vector<int> result;
+        for (const auto& node : nestedList)
+            if (node.isInteger()) {
+                result.push_back(node.getInteger());
+            } else {
+                vector<NestedInteger> sub_list = node.getList();
+                auto temp = flatten(sub_list);
+                result.insert(result.end(), temp.begin(), temp.end());
+            }
+
+        return result;
+    }
+};
