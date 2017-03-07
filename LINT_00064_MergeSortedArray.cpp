f3da1d8975d55f72c1cc7dc7839f6e9256316dd3
J.D <jordandong@gmail.com>
2017-03-07 16:51:37 +0800

    Update and rename MergeSortedArrayII.cpp to LINT_00064_MergeSortedArray.cpp

--- LINT_00064_MergeSortedArray.cpp
+++ LINT_00064_MergeSortedArray.cpp
@@ -1,13 +1,16 @@
 /*
 Given two sorted integer arrays A and B, merge B into A as one sorted array.
 
-Note
-You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are mand n respectively.
+Notice
+You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B.
+The number of elements initialized in A and B are m and n respectively.
 
 Example
-A = [1, 2, 3, empty, empty] B = [4,5]
+A = [1, 2, 3, empty, empty], B = [4, 5]
+After merge, A will be filled as [1, 2, 3, 4, 5]
 
-After merge, A will be filled as [1,2,3,4,5]
+Tags 
+Sorted Array Array Facebook
 */
 
 class Solution {
