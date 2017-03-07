d081214c1535623323799df7baa703eca00c1e4a
J.D <jordandong@gmail.com>
2017-03-07 00:20:16 -0800

    Update and rename StringToInteger(atoi).cpp to LINT_00054_StringToIntegerII.cpp

--- LINT_00054_StringToIntegerII.cpp
+++ LINT_00054_StringToIntegerII.cpp
@@ -1,5 +1,7 @@
 /*
-Implement atoi to convert a string to an integer. If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+Implement atoi to convert a string to an integer.
+If no valid conversion could be performed, a zero value is returned.
+If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
 
 Example
 "10" =>10
