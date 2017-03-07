7ece16d6d3c8259dd3663c592eec50b518948fcf
J.D <jordandong@gmail.com>
2017-03-07 16:38:53 +0800

    Update and rename LINT_00064_MergeSortedArray.cpp to LINT_00006_MergeTwoSortedArrays.cpp

--- LINT_00006_MergeTwoSortedArrays.cpp
+++ LINT_00006_MergeTwoSortedArrays.cpp
@@ -1,15 +1,17 @@
 /*
 Merge two given sorted integer array A and B into a new sorted integer array.
 
+Have you met this question in a real interview? Yes
 Example
 A=[1,2,3,4]
-
 B=[2,4,5,6]
-
 return [1,2,2,3,4,4,5,6]
 
-Challenge
+Challenge 
 How can you optimize your algorithm if one array is very large and the other is very small?
+
+Tags 
+Sorted Array Array
 */
 
 class Solution {
