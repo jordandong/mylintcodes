fc2bcd2f153ceba55aec4832c511293c48d24433
J.D <jordandong@gmail.com>
2017-10-03 22:57:29 +0800

    Create 00699_CheckSumOfKPrimes.cpp

---
+++ 00699_CheckSumOfKPrimes.cpp
@@ -0,0 +1,31 @@
+/*
+Given two numbers n and k. We need to find out if n can be written as sum of k prime numbers.
+
+ Notice
+
+n <= 10 ^ 9
+
+Goldbach's conjecture
+
+Have you met this question in a real interview? Yes
+Example
+Given n = 10, k = 2
+Return true   // 10 = 5 + 5
+
+Given n = 2, k = 2
+Return false
+Tags 
+Mathematics
+*/
+
+class Solution {
+public:
+    /*
+     * @param : an int
+     * @param : an int
+     * @return: if N can be expressed in the form of sum of K primes, return true; otherwise, return false.
+     */
+    bool isSumOfKPrimes(int n, int k) {
+        // write your code here
+    }
+};
