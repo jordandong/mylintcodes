58677cc97213718a96edfb162efa8398b8319be2
J.D <jordandong@gmail.com>
2017-10-05 06:35:37 -0500

    Create 00698_MaximumDifferenceInArrays.cpp

---
+++ 00698_MaximumDifferenceInArrays.cpp
@@ -0,0 +1,28 @@
+/*
+Given m arrays, and each array is sorted in ascending order. Now you can pick up two integers from two different arrays (each array picks one) and calculate the distance. We define the difference between two integers a and b to be their absolute difference |a-b|. Your task is to find the maximum difference.
+
+ Notice
+
+Each given array will have at least 1 number. There will be at least two non-empty arrays.
+The total number of the integers in all the m arrays will be in the range of [2, 10000].
+The integers in the m arrays will be in the range of [-10000, 10000].
+Have you met this question in a real interview? Yes
+Example
+Given arrays = [[1,2,3], [4,5], [1,2,3]]
+Return 4
+
+One way to reach the maximum difference 4 is to pick 1 in the first or third array and pick 5 in the second array.
+Tags 
+Hash Table Array Yahoo
+*/
+
+class Solution {
+public:
+    /*
+     * @param : an array of arrays
+     * @return: return the max distance among arrays
+     */
+    int maxDiff(vector<vector<int>> &arrs) {
+        // write your code here
+    }
+};
